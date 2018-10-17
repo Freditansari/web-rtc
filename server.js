0d4daacbf96d5f8a9a11c3cf7d60dc3a2cdf4e30
ftansari <freditansari@gmail.com>
2018-10-17 14:30:55 +0700

    local test is successful. deploying to heroku

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.get('/', function(req, res){
 });
 
 app.io.route('ready', function(req){
-    req.io.join(req.data)
+    req.io.join(req.data.chat_room);
+    req.io.join(req.data.signal_room);
     app.io.room(req.data).broadcast('announce',{
         message:'New client joined the '+ req.data+ ' room.'
     })
@@ -23,7 +24,15 @@ app.io.route('send', function(req){
     app.io.room(req.data.room).broadcast('message',{
         message:req.data.message,
         author: req.data.author
-    })
+    });
+})
+
+app.io.route('signal', function(req){
+    //req is used to the sender does not receive their own message
+    req.io.room(req.data.room).broadcast('signaling_message',{
+        type: req.data.type,
+        message:req.data.message
+    });
 })
 
 
